f35ec208dd9b00555bfe6e00ad9abe3c77b191da
“Joseph <akpannath84@gmail.com>
2025-02-02 03:13:15 -0800

    Fixed missing VALUES in task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,11 +1,11 @@
 INSERT INTO customers (customer_id, customer_name, email, address)
-SELECT 2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'
-WHERE NOT EXISTS (SELECT 1 FROM customers WHERE customer_id = 2);
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.')
+ON DUPLICATE KEY UPDATE customer_id = customer_id;
 
 INSERT INTO customers (customer_id, customer_name, email, address)
-SELECT 3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'
-WHERE NOT EXISTS (SELECT 1 FROM customers WHERE customer_id = 3);
+VALUES (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.')
+ON DUPLICATE KEY UPDATE customer_id = customer_id;
 
 INSERT INTO customers (customer_id, customer_name, email, address)
-SELECT 4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.'
-WHERE NOT EXISTS (SELECT 1 FROM customers WHERE customer_id = 4);
+VALUES (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.')
+ON DUPLICATE KEY UPDATE customer_id = customer_id;
